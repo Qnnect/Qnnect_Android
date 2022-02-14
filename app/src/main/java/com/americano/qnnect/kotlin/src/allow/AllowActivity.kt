aec4254ca8d4fcded1926082b401055f3d69e121
hwanggyuil <hwanggyuil@gmail.com>
2022-02-14 22:09:19 +0900

    profile layout commit

--- app/src/main/java/com/americano/qnnect/kotlin/src/allow/AllowActivity.kt
+++ app/src/main/java/com/americano/qnnect/kotlin/src/allow/AllowActivity.kt
@@ -1,8 +1,10 @@
 package com.americano.qnnect.kotlin.src.allow
 
+import android.content.Intent
 import android.os.Bundle
 import com.americano.qnnect.kotlin.config.BaseActivity
 import com.americano.qnnect.kotlin.databinding.ActivityAllowBinding
+import com.americano.qnnect.kotlin.src.profile.ProfileActivity
 
 class AllowActivity : BaseActivity<ActivityAllowBinding>(ActivityAllowBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +14,11 @@ class AllowActivity : BaseActivity<ActivityAllowBinding>(ActivityAllowBinding::i
             binding.allowRadio1.isSelected = true
             binding.allowRadio2.isSelected = true
             binding.allowRadio3.isSelected = true
+        }
+
+        binding.okBtn.setOnClickListener { 
+            var intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/americano/qnnect/kotlin/src/profile/ProfileActivity.kt
+++ app/src/main/java/com/americano/qnnect/kotlin/src/profile/ProfileActivity.kt
@@ -1,13 +1,10 @@
 package com.americano.qnnect.kotlin.src.profile
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.americano.qnnect.kotlin.R
 import com.americano.qnnect.kotlin.config.BaseActivity
-import com.americano.qnnect.kotlin.databinding.ActivityMainBinding
-import com.americano.qnnect.kotlin.src.main.home.HomeFragment
+import com.americano.qnnect.kotlin.databinding.ActivityProfileBinding
 
-class ProfileActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class ProfileActivity : BaseActivity<ActivityProfileBinding>(ActivityProfileBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
