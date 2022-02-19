bc610780f1d4c9314b7779171ddfa9f66137cbdc
hwanggyuil <hwanggyuil@gmail.com>
2022-02-20 00:36:12 +0900

    bottom sheet update

---
+++ app/src/main/java/com/iame/qnnect/android/viewmodel/GroupBottomViewModel.kt
@@ -0,0 +1,9 @@
+package com.iame.qnnect.android.viewmodel
+
+import com.iame.qnnect.android.base.BaseViewModel
+
+class GroupBottomViewModel() : BaseViewModel() {
+
+    private val TAG = "HomeViewModel"
+
+}
