24cb275952798c8dcc440e0009463b61ce358c10
hwanggyuil <hwanggyuil@gmail.com>
2022-04-20 19:49:08 +0900

    base change

--- app/src/main/java/com/iame/qnnect/android/base/BaseViewModel.kt
+++ app/src/main/java/com/iame/qnnect/android/base/BaseViewModel.kt
@@ -15,12 +15,6 @@ import what.the.mvvm.util.SnackbarMessageString
 
 
 open class BaseViewModel : ViewModel() {
-
-    // 일회성 이벤트를 만들어 내는 라이브 이벤트
-    // 뷰는 이러한 이벤트를 바인딩하고 있다가, 적절한 상황이 되면 액티비티를 호출하거나 스낵바를 만듬
-    private val snackbarMessage = SnackbarMessage()
-    private val snackbarMessageString = SnackbarMessageString()
-
     /**
      * RxJava 의 observing을 위한 부분.
      * addDisposable을 이용하여 추가하기만 하면 된다
@@ -39,15 +33,4 @@ open class BaseViewModel : ViewModel() {
         compositeDisposable.clear()
         super.onCleared()
     }
-
-    /**
-     * 스낵바를 보여주고 싶으면 viewModel에서 이 함수를 호출
-     */
-    fun showSnackbar(stringResourceId:Int) {
-        snackbarMessage.value = stringResourceId
-    }
-    fun showSnackbar(str:String){
-        snackbarMessageString.value = str
-    }
-
 }
